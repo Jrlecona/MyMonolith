913a1be0c9603f82f2c33806f8ca86dab676c6c9
Ruben Lecoña <jlecona@dollargeneral.com>
2025-04-10 20:45:02 -0400

    add controllers

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using MyMonolith.Data;
+using MyMonolith.Models;
+
+namespace MyMonolith.Controllers;
+
+[ApiController]
+[Route("[api/products]")]
+public class ProductController : Controller
+{
+    private readonly StoreContext _context;
+    private readonly ILogger<ProductController> _logger;
+
+    public ProductController(ILogger<ProductController> logger, StoreContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GetProducts()
+    {
+        var products = _context.Products.ToList();
+        return Ok(products);
+    }
+
+    [HttpPost]
+    public IActionResult AddProduct([FromBody] Product product)
+    {
+        _context.Products.Add(product);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(GetProducts), new { id = product.Id }, product);
+    }
+
+    // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    // public IActionResult Error()
+    // {
+    //     return View("Error!");
+    // }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using MyMonolith.Data;
+using MyMonolith.Models;
+
+namespace MyMonolith.Controllers;
+
+[ApiController]
+[Route("[api/users]")]
+public class UserController : Controller
+{
+    private readonly StoreContext _context;
+    private readonly ILogger<UserController> _logger;
+
+    public UserController(ILogger<UserController> logger, StoreContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GetUsers()
+    {
+        var users = _context.User.ToList();
+        return Ok(users);
+    }
+
+    [HttpPost]
+    public IActionResult AddUser([FromBody] User user)
+    {
+        user.CreatedAt = DateTime.UtcNow;
+        _context.User.Add(user);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(GetUsers), new { id = user.Id }, user);
+    }
+
+    // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    // public IActionResult Error()
+    // {
+    //     return View("Error!");
+    // }
+}
